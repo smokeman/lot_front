d95330be0ff4717bbd123be5580b4caab6e7f831
1 <99762276@qq.com>
2017-10-06 19:24:33 +0800

    f-1006-6

---
+++ src/components/wine/ok.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        完成存酒
+    </div>
+</template>
--- src/router/router.config.js
+++ src/router/router.config.js
@@ -8,12 +8,14 @@ import LuckerList from '../components/my/LuckerList.vue'
 import CouponList from '../components/my/CouponList.vue'
 import WineMch from '../components/wine/WineMch.vue'
 import WinePlayer from '../components/wine/WinePlayer.vue'
+import ok from '../components/wine/ok.vue'
 
 let routes = [
         { path: '/', redirect: '/setting', meta: { keepAlive: false, title: '抽奖' } },
         { path: '/lottery', component: Lottery, meta: { keepAlive: false, title: '抽奖' } },
         { path: '/wine_mch', component: WineMch, meta: { keepAlive: false, title: '存酒商家' } },
         { path: '/wine_player', component: WinePlayer, meta: { keepAlive: false, title: '存酒玩家' } },
+        { path: '/wine_ok', component: ok, meta: { keepAlive: false, title: '存酒完成' } },
         // { path: '/news', component: News, meta: { keepAlive: false, title: '网易 - 头条新闻' } },
         { path: '/userset', component: Lottery },
         // { path: '/MouserData', component: MouserData },
